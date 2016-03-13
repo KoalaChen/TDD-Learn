c0833648e7ad99d07a285dfbf6914279af9a8256
paul <taiwantom20536@gmail.com>
2016-03-13 12:40:31 +0800

    寫"測試案例2" Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190

--- Day2/PotterShoppingCart.Tests/OrderServiceTest.cs
+++ Day2/PotterShoppingCart.Tests/OrderServiceTest.cs
@@ -55,5 +55,44 @@ namespace PotterShoppingCart.Tests
             //檢查訂單的Total是否為預期結果
             Assert.AreEqual(expected, actual.Total); 
         }
+        //Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190
+        [TestMethod]
+        public void Checkout_買Book1一本和Book2一本_Price應為190元()
+        {
+            //Arrange
+            var book1 = new Product() //Book1產品內容
+            {
+                Name = "哈利波特第一集",
+                Price = 100
+            };
+            var book2 = new Product() //Book2產品內容
+            {
+                Name = "哈利波特第二集",
+                Price = 100
+            };
+            var orderInfoList = new OrderDetail[] {
+                new OrderDetail() //訂單細項，買Book1 1本
+                {
+                    Product = book1, //購買Book1
+                    Quantity = 1 //購買數量
+                },
+                new OrderDetail()
+                {
+                    Product = book2, //購買Book2
+                    Quantity = 1 //購買數量
+                }
+            };
+
+            var target = new OrderService(); //訂單計算服務
+            var expected = 190; //預期結果為190元
+
+            //Act
+            //進行結帳動作，產生訂單，並計算結果
+            Order actual = target.Checkout(orderInfoList);
+
+            //Assert
+            //檢查訂單的Total是否為預期結果
+            Assert.AreEqual(expected, actual.Total);
+        }
     }
 }
